0116744351fa904a1798a0ce379ebfd50fd41cad
Danarfadila <danar.duha.fadila@gmail.com>
2024-03-13 19:57:53 +0700

    add font and colors on twconfig,sliceui

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,20 @@ module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        cookWiz: ["cookWiz", "cursive"],
+      },
+      colors: {
+        "cw-background": "#261F1D",
+        "cw-primary": "#6F645D",
+        "cw-secondary": "#DAD0C4",
+        "cw-tertiary": "#A69990",
+      },
+    },
+  },
   plugins: [require("daisyui")],
 };
