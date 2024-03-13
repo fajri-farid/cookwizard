3f5f3eab8575b2d76f77d5f386ef64be12bc905b
fajri-farid <fajrijry14@gmail.com>
2024-03-13 16:47:13 +0800

    api ai sudah terhubung

---
+++ src/app/api/v1/ai/route.js
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import OpenAI from "openai";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req) {
+  const { prompt } = await req.json();
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [
+      {
+        role: "system",
+        content:
+          "You are an cooking expert and able to give me dishes recipe from all the world",
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  return NextResponse.json({ data: response });
+}
